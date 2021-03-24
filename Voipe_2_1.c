78114d9e4bea7dee09f21204fc2d782078434147
Dongbin Yang <yangdongbin@hotmail.com>
2021-03-24 21:02:50 +0200

    Update Voipe_2_1.c

--- Voipe_2_1.c
+++ Voipe_2_1.c
@@ -1,5 +1,6 @@
-Write a functioning program in C that, upon execution, prints the following on the screen: "Hello everybody!", 
+/*Write a functioning program in C that, upon execution, prints the following on the screen: "Hello everybody!", 
 followed by a newline character \n after the exclamation point. After printing, the program closes itself.
+*/
 
 #include <stdio.h>
 void main (void){
